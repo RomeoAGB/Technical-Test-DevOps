2b8f6f9b6957a16f9a18eeb390b0f2d38c5f6420
RomeoAGB <45411050+RomeoAGB@users.noreply.github.com>
2025-02-18 17:27:26 -0600

    Add files via upload

--- ejemplo_py.py
+++ ejemplo_py.py
@@ -1,11 +1,17 @@
+"""
+Función para saludar a un usuario.
+"""
 
 def saludar(nombre: str) -> str:
+    """Retorna un saludo con nombre personalizado"""
     return f"¡Hola, {nombre}!"
 
 def main():
+    """Imprime un saludo."""
     nombre = "Romeo"
     saludo = saludar(nombre)
     print(saludo)
 
 if __name__ == "__main__":
     main()
+
